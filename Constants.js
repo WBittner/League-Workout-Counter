c9f639e5fcb0374d8ff2b419b93ea74fda620f5d
William Bittner <wjb5174@psu.edu>
2016-07-31 14:37:52 -0400

    Modify how constants are added to allow the building of constants upon previous constants

--- Constants.js
+++ Constants.js
@@ -3,15 +3,23 @@
 *	Constant.js - defined constants for use through out the codebase.
 *
 */
+var constants = {};
 
-module.exports = 
-{
-	API_KEY: "6664f4db-4da8-4d3b-a858-20dc58175f06",
-	API_KEY_URL_FORMATTED: "?api_key=6664f4db-4da8-4d3b-a858-20dc58175f06",
-	URL_BASE: "https://na.api.pvp.net/api/lol/na/",
-	VERSIONS: 
-	{
-		SUMMONER: "v1.4/"
-	},
-	SUMMONER_URL: "summoner/by-name/"
-}
+constants.API_KEY = "6664f4db-4da8-4d3b-a858-20dc58175f06";
+
+constants.VERSIONS = {};
+constants.VERSIONS.SUMMONER = "v1.4";
+constants.VERSIONS.GAME = "v1.3";
+
+constants.URLS = {};
+constants.URLS.BASE = "https://na.api.pvp.net/api/lol/na/";
+constants.URLS.API_KEY = "?api_key=" + constants.API_KEY;
+constants.URLS.ENDPOINTS = {};
+constants.URLS.ENDPOINTS.SUMMONER = "summoner/by-name/";
+constants.URLS.ENDPOINTS.GAME = "game/by-summoner/";
+constants.URLS.SUMMONER = constants.URLS.BASE + constants.VERSIONS.SUMMONER + "/" + constants.URLS.ENDPOINTS.SUMMONER + "%s" + constants.URLS.API_KEY;
+constants.URLS.GAME = constants.URLS.BASE + constants.VERSIONS.GAME +  + "/" + constants.URLS.ENDPOINTS.GAME + "%s/recent" + constants.URLS.API_KEY;
+
+
+
+module.exports = constants;
