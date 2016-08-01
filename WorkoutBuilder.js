ace0093916bc562625a2d5fad9cb5ed92623a467
William Bittner <wjb5174@psu.edu>
2016-07-31 21:07:51 -0400

    Add KDA extraction from the match history in workout builder

--- WorkoutBuilder.js
+++ WorkoutBuilder.js
@@ -5,11 +5,45 @@
 */
 module.exports = function(matchHistoryJSON)
 {
+	//Grab all the games from the day of the last match
 	const todaysGames = getTodaysGames(matchHistoryJSON);
 
-	return todaysGames;
+	//Get the KDA of last game and KDA of toadays losses.
+	const KDAs = getKDAs(todaysGames);
+
+	return KDAs;
 }
 
+function getKDAs(games)
+{
+	//Return KDAs from the last game and the summation KDA of all games lost
+	var KDAs = {};
+
+	const lastGameStats = games.pop().stats; //games we added to the array from oldest to newest, so popping will give us the most recently played game
+	KDAs.lastGame = {};
+	KDAs.lastGame.won = lastGameStats.win;
+	KDAs.lastGame.K = lastGameStats.championsKilled;
+	KDAs.lastGame.D = lastGameStats.numDeaths;
+	KDAs.lastGame.A = lastGameStats.assists;
+
+	KDAs.otherGames = {};
+	KDAs.otherGames.K = 0;
+	KDAs.otherGames.D = 0;
+	KDAs.otherGames.A = 0;
+
+	for(var i = 0; i < games.length; i++)
+	{
+		var gameStats = games[i].stats;
+		if(gameStats.win == false)
+		{
+			KDAs.otherGames.K += gameStats.championsKilled;
+			KDAs.otherGames.D += gameStats.numDeaths;
+			KDAs.otherGames.A += gameStats.assists;
+		}
+	}
+
+	return KDAs;
+}
 
 function getTodaysGames(matchHistoryJSON)
 {
@@ -19,7 +53,7 @@ function getTodaysGames(matchHistoryJSON)
 	const games = matchHistoryJSON.games;
 	const dateOfLastMatch = new Date(games[0].createDate);
 	var todaysGames = [];
-	for(var i = 0; i < games.length; i++)
+	for(var i = games.length-1; i >= 0; i--)
 	{
 		var game = games[i];
 		var tempDate = new Date(game.createDate);
